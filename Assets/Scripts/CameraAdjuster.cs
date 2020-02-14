124853363023dee216787e584373ad9e9c395fb5
hama1185 <hamataku7332@softbank.ne.jp>
2020-02-14 11:12:28 +0900

    回転を反転

--- Assets/Scripts/CameraAdjuster.cs
+++ Assets/Scripts/CameraAdjuster.cs
@@ -6,6 +6,20 @@ using UnityEngine.UI;
 public class CameraAdjuster : MonoBehaviour {
     public Quaternion sentAngle{get;set;} = new Quaternion(0,0,0,1);
     public void Adjust(){
-        this.transform.localRotation = sentAngle;
+        // eulerX = 360.0f - eulerX;	
+        // if (eulerX > 180.0f) {	
+        //     eulerX -= 360.0f;	
+        // }	
+
+        // eulerY = 360.0f - eulerY;	
+        // if (eulerY > 180.0f) {	
+        //     eulerY -= 360.0f;	
+        // }	
+
+        // eulerZ = 360.0f - eulerZ;	
+        // if (eulerZ > 180.0f) {	
+        //     eulerZ -= 360.0f;	
+        // }
+        this.transform.localRotation = Quaternion.Euler(-sentAngle.eulerAngles.x, -sentAngle.eulerAngles.y, sentAngle.eulerAngles.z);
     }
 }
