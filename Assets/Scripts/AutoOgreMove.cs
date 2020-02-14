819e60a2812878efeab28f2e25f295365ac99cb8
toneDrei79 <tone0709@icloud.com>
2020-02-14 14:58:33 +0900

    ogreの移動を修正

--- Assets/Scripts/AutoOgreMove.cs
+++ Assets/Scripts/AutoOgreMove.cs
@@ -7,12 +7,12 @@ public class AutoOgreMove : MonoBehaviour {
 
     void Start() {
         ogre = GameObject.FindWithTag("Enemy").transform;
-        ogre.position = new Vector3(0.0f, 1.0f, 51.0f);
+        ogre.position = new Vector3(0.0f, 1.0f, -51.0f);
     }
 
     void Update() {
-        if (ogre.position.z > 5.0f) {
-            ogre.position -= new Vector3(0.0f, 0.0f, 2.0f * Time.deltaTime);
+        if (ogre.position.z < -5.0f) {
+            ogre.position += new Vector3(0.0f, 0.0f, 2.0f * Time.deltaTime);
         }
     }
 }
